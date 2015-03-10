d764b0e3ef261c8fc9d551009fa359323d19854e
ilker <ilkerksn@gmail.com>
2015-03-10 03:23:45 +0200

    local_settings.py.example added

--- ituro/ituro/settings.py
+++ ituro/ituro/settings.py
@@ -81,3 +81,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Import local settings
+try:
+    from local_settings import *
+except ImportError:
+    pass
