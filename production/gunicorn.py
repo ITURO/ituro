7ed887838b14387baead0b02cb3db6dc8c207210
Ilker Kesen <ilkerksn@gmail.com>
2015-03-15 13:27:15 -0400

    ready for production, go go go

--- production/gunicorn.py
+++ production/gunicorn.py
@@ -1,3 +1,3 @@
-command = "/root/envs/ituro/bin/gunicorn"
-pythonpath = "/root/apps/ituro/ituro"
+command = "/web/envs/ituro/bin/gunicorn"
+pythonpath = "/web/apps/ituro/ituro"
 bind = "127.0.0.1:8000"
