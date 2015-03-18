d493cff71584ceb8f15b1259ecbc67800a2c74d1
ilker <ilkerksn@gmail.com>
2015-03-18 19:03:46 +0200

    project manager added to list_display in admin site

--- ituro/projects/admin.py
+++ ituro/projects/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 from projects.models import Project, Membership
 
 
@@ -7,11 +8,18 @@ class MembershipInlineAdmin(admin.TabularInline):
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category')
+    list_display = ('name', 'category', 'manager')
     list_filter = ('category',)
     default_filters = ('is_active=True',)
     search_fields = ('name',)
     inlines = [MembershipInlineAdmin]
+
+    def manager(self, obj):
+        manager = obj.membership_set.filter(is_manager=True)[0].member
+        return '<a href="{}">{}</a>'.format(
+            reverse('admin:accounts_customuser_change', args=[manager.pk]),
+            manager.email)
+    manager.allow_tags = True
 
 
 admin.site.register(Project, ProjectAdmin)
