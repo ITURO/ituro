69dee7ac7e9346d4e63987474757b2cdce58f672
Tolga <bilbeyt@itu.edu.tr>
2016-04-03 10:45:53 +0300

    Qrcode referee parse validation completed

--- ituro/referee/forms.py
+++ ituro/referee/forms.py
@@ -1,10 +1,41 @@
 from django import forms
+from projects.models import Project
 from django.utils.translation import ugettext_lazy as _
 
 
 class QRCodeCheckForm(forms.Form):
 
-    user_qrcode=forms.CharField(max_length=50, required=True,
-                                label=_("User QRCode"))
-    project_qrcode=forms.CharField(max_length=50, required=True,
-                                   label=_("Project QRCode"))
+    user_qrcode = forms.CharField(max_length=50, required=True,
+                                  label=_("User QRCode"))
+    project_qrcode = forms.CharField(max_length=50, required=True,
+                                     label=_("Project QRCode"))
+
+    def clean_user_qrcode(self):
+        qrcode = str(self.cleaned_data.get("user_qrcode")).split("-")
+        error = False
+        if len(qrcode) != 2:
+            raise forms.ValidationError(_("User Qrcode format is wrong."))
+        for number in qrcode:
+            if not number.isdigit():
+                error = True
+        if error:
+            raise forms.ValidationError(_("User Qrcode must have 2 integer."))
+        return qrcode
+
+    def clean_project_qrcode(self):
+        qrcode = str(self.cleaned_data.get("project_qrcode")).split("-")
+        error = False
+        if len(qrcode) != 4:
+            raise forms.ValidationError(_("Project Qrcode format is wrong."))
+        for i in range(0, len(qrcode)):
+            if i == 2:
+                if not Project.objects.filter(category=qrcode[i]).exists():
+                    raise forms.ValidationError(_("There is no category for \
+                                                given project qrcode"))
+            else:
+                if not qrcode[i].isdigit():
+                    error = True
+        if error:
+            raise forms.ValidationError(_("Project Qrcode must contain 3 \
+                                        integer"))
+        return qrcode
