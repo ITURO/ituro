d1c86c7275e8ab12a1fdaa06fbdbba798c4fac70
Tolga <bilbeyt@itu.edu.tr>
2016-04-08 11:50:12 +0300

    fixrankings error fixed

--- ituro/sumo/management/commands/fixrankings.py
+++ ituro/sumo/management/commands/fixrankings.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for group in SumoGroup.objects.all():
-            order = 0
             query = SumoGroupTeam.objects.filter(group=group)
+            order = 0
             for i in range(0, len(query)):
                 if query[i].order == 0:
                     rivals_check = SumoGroupTeam.objects.filter(group=query[i].group,
@@ -22,7 +22,8 @@ class Command(BaseCommand):
                         rival_count = rivals.count()
                         if rival_count == 1:
                             rival = rivals.first()
-                            order = check_double_average(query[i], rival, order)
+                            context = check_double_average(query[i], rival, order)
+                            order = context["order"]
                             query = SumoGroupTeam.objects.filter(group=group)
                         elif rival_count == 2:
                             print "Triple average occured in group {}".format(group)
@@ -41,18 +42,18 @@ class Command(BaseCommand):
 
 
 def check_double_average(robot, rival, order):
-    home_check = SumoGroupMatch.objects.filter(home=robot, away=rival).exists()
-    away_check = SumoGroupMatch.objects.filter(home=rival, away=robot).exists()
+    home_check = SumoGroupMatch.objects.filter(home=robot.robot, away=rival.robot).exists()
+    away_check = SumoGroupMatch.objects.filter(home=rival.robot, away=robot.robot).exists()
     robot_average = 0
     rival_average = 0
-
+    context = dict()
     if home_check:
-        matches = SumoGroupMatch.objects.filter(home=robot, away=rival)
+        matches = SumoGroupMatch.objects.filter(home=robot.robot, away=rival.robot)
         for match in matches:
             robot_average += match.home_score
             rival_average += match.away_score
     elif away_check:
-        matches = SumoGroupMatch.objects.filter(home=rival, away=robot)
+        matches = SumoGroupMatch.objects.filter(home=rival.robot, away=robot.robot)
         for match in matches:
             robot_average += match.away_score
             rival_average += match.home_score
@@ -63,7 +64,8 @@ def check_double_average(robot, rival, order):
         order += 1
         rival.order = order
         rival.save()
-        return order
+        context["order"] = order
+        return context
     elif rival_average > robot_average:
         order += 1
         rival.order = order
@@ -71,4 +73,5 @@ def check_double_average(robot, rival, order):
         order += 1
         robot.order = order
         robot.save()
-        return order
+        context["order"] = order
+        return context
