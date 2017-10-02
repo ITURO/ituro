c16006cd8983bbd73f52921c63a51aa6f29b9e88
alperkesen <kesen.alper@gmail.com>
2017-10-03 01:30:16 +0300

    Add test for creating accounts

--- ituro/accounts/tests.py
+++ ituro/accounts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.utils import timezone
+from accounts.models import CustomUser, CustomUserManager
 
-# Create your tests here.
+
+class UserCreateTestCase(TestCase):
+    def test_create_user_correctly(self):
+        "Creating users correctly"
+
+        new_user = CustomUser.objects.create(
+            email="participant@gmail.com",
+            name="Participant Name",
+            phone="09876543210",
+            school="Some University",
+            is_staff="False",
+            is_active="True",
+            date_joined=timezone.now())
+
+        self.assertTrue(isinstance(new_user, CustomUser))
+        self.assertEqual(new_user.get_full_name(), "Participant Name")
+        self.assertEqual(new_user.get_short_name(), "Participant Name")
