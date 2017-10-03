08cfd5a5be4bcbc12b9ec62f9e942533bdba8894
alperkesen <kesen.alper@gmail.com>
2017-10-03 14:54:55 +0300

    Add testcase for generating line follower orders

--- ituro/orders/tests.py
+++ ituro/orders/tests.py
@@ -4,16 +4,17 @@ from django.utils import timezone
 from django.core.management import call_command
 from accounts.models import CustomUser, CustomUserManager
 from projects.models import Project
-from orders.models import RaceOrder
+from orders.models import RaceOrder, LineFollowerRaceOrder, LineFollowerStage
+from results.models import LineFollowerResult
+from random import randint
+from math import ceil
 
 
 class GenerateOrdersTestCase(TestCase):
-    def test_stairclimbing_generate_orders(self):
+    def test_stair_climbing_generate_orders(self):
         "Testing generating order for stair climbing category"
 
         num_users = 10
-        users = []
-        projects = []
 
         for i in range(num_users):
             user = CustomUser.objects.create(
@@ -21,23 +22,18 @@ class GenerateOrdersTestCase(TestCase):
                 name="Participant " + str(i),
                 phone="90543125413" + str(i),
                 school="ITU",
-                is_staff="False",
-                is_active="True",
+                is_staff=False,
+                is_active=True,
                 date_joined=timezone.now())
 
-            users.append(user)
-
-        for i in range(num_users):
             project = Project.objects.create(
-                manager=users[i],
+                manager=user,
                 category="stair_climbing",
                 name="Climber " + str(i),
                 is_confirmed=True,
                 created_at=timezone.now())
 
-            projects.append(project)
-
-        for project in projects:
+        for project in Project.objects.all():
             self.assertFalse(RaceOrder.objects.filter(
                 project_id=project.id).exists())
 
@@ -46,6 +42,81 @@ class GenerateOrdersTestCase(TestCase):
         self.assertEqual(out.getvalue(), "Race orders generated for "
                          "stair_climbing category.\n")
 
-        for project in projects:
+        for project in Project.objects.all():
             self.assertTrue(RaceOrder.objects.filter(
                 project_id=project.id).exists())
+
+    def test_line_follower_generate_orders(self):
+        "Testing generating order for line follower category"
+
+        num_users = 10
+
+        for i in range(num_users):
+            user = CustomUser.objects.create(
+                email="participant" + str(i) + "@gmail.com",
+                name="Participant " + str(i),
+                phone="90543125413" + str(i),
+                school="ITU",
+                is_staff=False,
+                is_active=True,
+                date_joined=timezone.now())
+
+            project = Project.objects.create(
+                manager=user,
+                category="line_follower",
+                name="Line Follower " + str(i),
+                is_confirmed=True,
+                created_at=timezone.now())
+
+        stage1 = LineFollowerStage.objects.create(
+            order=1,
+            is_current=True,
+            is_final=False,
+            orders_available=False,
+            results_available=False)
+
+        self.assertTrue(isinstance(stage1, LineFollowerStage))
+
+        for project in Project.objects.all():
+            self.assertFalse(LineFollowerRaceOrder.objects.filter(
+                project_id=project.id, stage=stage1).exists())
+
+        out = StringIO()
+        call_command('linefollowergenerateorders', '1', stdout=out)
+        self.assertEqual(out.getvalue(), "Line follower race orders "
+                         "generated for day #1.\n")
+
+        for project in Project.objects.all():
+            self.assertTrue(LineFollowerRaceOrder.objects.filter(
+                project_id=project.id, stage=stage1).exists())
+
+            result = LineFollowerResult.objects.create(
+                project=project,
+                minutes=randint(1, 5),
+                seconds=randint(0, 59),
+                milliseconds=randint(0, 999),
+                disqualification=False,
+                is_best=True,
+                created_at=timezone.now(),
+                stage=stage1,
+                runway_out=randint(0, 5))
+
+        stage2 = LineFollowerStage.objects.create(
+            order=2,
+            is_current=True,
+            is_final=True,
+            orders_available=False,
+            results_available=False)
+
+        for project in Project.objects.all():
+            self.assertFalse(LineFollowerRaceOrder.objects.filter(
+                project_id=project.id, stage=stage2).exists())
+
+        out = StringIO()
+        call_command('linefollowergenerateorders', '2', stdout=out)
+        self.assertEqual(out.getvalue(), "Line follower race orders "
+                         "generated for day #2.\n")
+
+        self.assertEqual(ceil(len(Project.objects.all()) * 0.4),
+                         len(LineFollowerRaceOrder.objects.filter(
+                             stage=stage2)))
