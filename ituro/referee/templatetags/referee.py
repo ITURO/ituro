b1ed17a9cdc4e281279af4dc53e475c82ff73764
batuortal <batuhanortal@gmail.com>
2019-04-11 06:55:08 +0300

    Removed Line Follower from Referee Templatetags

--- ituro/referee/templatetags/referee.py
+++ ituro/referee/templatetags/referee.py
@@ -1,21 +1,8 @@
 from django import template
-from orders.models import LineFollowerStage, LineFollowerJuniorStage
-from results.models import LineFollowerResult, LineFollowerJuniorResult
+from orders.models import LineFollowerJuniorStage
+from results.models import LineFollowerJuniorResult
 
 register = template.Library()
-
-
-@register.inclusion_tag("referee/line_follower_actions.html")
-def line_follower_actions(stage_order, project_id):
-    results = LineFollowerResult.objects.filter(
-        stage__order=stage_order, project__id=project_id)
-    return {"results": results}
-
-
-@register.simple_tag
-def line_follower_result_count(stage_order, project_id):
-    return LineFollowerResult.objects.filter(
-        stage__order=stage_order, project__id=project_id).count()
 
 
 @register.inclusion_tag("referee/line_follower_junior_actions.html")
